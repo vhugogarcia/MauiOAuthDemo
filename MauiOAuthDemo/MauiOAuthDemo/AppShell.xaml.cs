5f10e1e8a240e11d0a19e4df396b2d660569bcd8
Victor H Garcia <ihugo@live.com>
2023-03-09 11:51:54 -0600

    Created a repo to demo the crash of the webauthenticator on Android when setting a different browser as default

---
+++ MauiOAuthDemo/MauiOAuthDemo/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiOAuthDemo;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
+
---
+++ MauiOAuthDemo/MauiOAuthDemo/Constants.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace MauiOAuthDemo
+{
+    public class Constants
+    {
+        public const string CallBackUrl = "mauioauthdemo://callback";
+        public const string Domain = "dev-eg83cemysc5nsi6f.us.auth0.com";
+        public const string ClientId = "PoB91CGAhaIa4ooPXTXZiu6DRNZztVuc";
+        public const string ClientSecret = "QzmaAnNLkweKU0zyUsRfh0vFeM31KerK3GQ0asMM7jq-zxXfpvlByCsBlQ4Mb0vY";
+    }
+}
+
